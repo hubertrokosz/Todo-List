b077955bf770e1c095098acc8489f377a242e12b
Hubert <142225610+hubertrokosz@users.noreply.github.com>
2024-03-17 23:03:01 +0100

    Set the foundation for my project using webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      open: true, // Automatically open the browser
+      port: 3000, // Specify a port to run on
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+    ],
+  },
+};
